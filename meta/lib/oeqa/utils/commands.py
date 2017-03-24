00b19f1c84f484676f1aaa04a300c5a51f9ddd42
Patrick Ohly <patrick.ohly@intel.com>
2017-03-24 17:57:22 +0100

    oeqa: allow extending qemuparams="..."
    Sometimes it is useful to reconfigure the qemu virtual machine
    directly. runqemu has the "qemuparams" parameter for that, and the
    underlying start() methods also supported modifying that via their
    "params" parameter. Only the runqemu() wrapper function lacked
    a way to specify additional parameters.
    
    One potential usage is to attach additional disks.
    
    Signed-off-by: Patrick Ohly <patrick.ohly@intel.com>

--- meta/lib/oeqa/utils/commands.py
+++ meta/lib/oeqa/utils/commands.py
@@ -218,7 +218,7 @@ def create_temp_layer(templayerdir, templayername, priority=999, recipepathspec=
 
 
 @contextlib.contextmanager
-def runqemu(pn, ssh=True, runqemuparams='', image_fstype=None):
+def runqemu(pn, ssh=True, runqemuparams='', image_fstype=None, qemuparams=None):
 
     import bb.tinfoil
     import bb.build
@@ -260,7 +260,7 @@ def runqemu(pn, ssh=True, runqemuparams='', image_fstype=None):
     try:
         qemu.deploy()
         try:
-            qemu.start(ssh=ssh, runqemuparams=runqemuparams)
+            qemu.start(params=qemuparams, ssh=ssh, runqemuparams=runqemuparams)
         except bb.build.FuncFailed:
             raise Exception('Failed to start QEMU - see the logs in %s' % logdir)
 
